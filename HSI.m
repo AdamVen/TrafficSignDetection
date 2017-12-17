bf08a2a770f83efff358b83d7f73820945764b3f
AdamVen <33135394+AdamVen@users.noreply.github.com>
2017-12-16 17:27:40 -0800

    Add files via upload

---
+++ HSI.m
@@ -0,0 +1,19 @@
+% House Keeping
+home;
+close all;
+clear;
+
+tic;
+
+TrafficSigns('stopsign.jpg', 8);
+TrafficSigns('stopsign2.jpg', 8);
+TrafficSigns('stopsign3.jpg', 8);
+TrafficSigns('stopsign4.jpg', 8);
+TrafficSigns('stopsign5.jpg', 8);
+TrafficSigns('stopsign6.jpg', 8);
+TrafficSigns('stopsign7.jpg', 8);
+TrafficSigns('trianglesign.jpg', 3);
+TrafficSigns('apples.jpg', 8);
+TrafficSigns('IMG_5224.jpg', 8);
+
+toc;
